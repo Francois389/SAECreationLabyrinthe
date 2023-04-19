f6250924ed589650a7466ea83a83e352b4a00470
François SP <francois-sp@gmx.fr>
2023-04-19 15:32:44 +0200

    Développement des la classe Graphe

--- Labyrinthe/src/representation/Graphe.java
+++ Labyrinthe/src/representation/Graphe.java
@@ -1,23 +1,64 @@
 package representation;
 
-import  representation.Sommet;
+//import  representation.Sommet;
 
+/**
+ * 
+ * //TODO Commenter la responsabilités de la classe Graphe
+ * @author Quentin Costes
+ * @author François de Saint Palais
+ *
+ */
 public class Graphe {
 
+    //TODO des que la classe Sommet est fini changer les tableau de int en tableau de Sommet
     /** liste de tous le sommets */
-    private Sommet[] listeSommet;
+    private int[] listeSommet;
     
     /** liste de tous le sommets */
-    private Sommet[][] listeArcs;
+    private int[][] listeArcs;
     
     
-	public Graphe(Sommet[] listeSommets, Sommet[][] listeArcs) {
+    /**
+     * Créer un graphe composé 
+     * @param listeSommets
+     * @param listeArcs
+     */
+	public Graphe(int[] listeSommets, int[][] listeArcs) {
         super();
+        
+        if (!estValide(listeSommets,listeArcs)) {
+            throw new IllegalArgumentException("Erreur - Paramétre du constructeur invalide");
+        }
+        
+        
+        
         this.listeSommet = listeSommets;
         this.listeArcs = listeArcs;
     }
     
-	/**
+	private boolean estValide(int[] listeSommets, int[][] listeArcs) {
+        boolean valide;
+        
+        if (listeSommets == null && listeArcs == null) {
+            return false;
+        }
+        valide = false;
+        for (int i = 0; i < listeSommets.length; i++ ){
+            for (int j = 0; j < listeArcs.length; j++) {
+                if (listeArcs[j][0] == listeSommets[i] || listeArcs[j][1] == listeSommets[i]) {
+                     valide = true;
+                 }   
+            }
+            if (!valide){
+                return false;
+            }
+        }
+        
+        return true;
+    }
+
+    /**
 	 * renvoie le nombre d'arcs du graphe
 	 * @return int nombre d'arcs
 	 */
--- Labyrinthe/src/test/representation/TestGraphe.java
+++ Labyrinthe/src/test/representation/TestGraphe.java
@@ -2,23 +2,69 @@ package test.representation;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+
+import representation.Graphe;
+
+
+/**
+ * Test Unitaire de la classe Graphe
+ * @author Costes Quentin
+ * @author François de Saint Palais
+ * 
+ *
+ */
 class TestGraphe {
 
     @Test
-    void testGraphe() {
-        fail("Not yet implemented");
+    @DisplayName("Test du constructeur avec valeur Valide")
+    void testGrapheOK() {
+        {
+            int[] tS = {1, 2, 3};
+            int[][] tA = {{1, 2}, {2, 3}};
+            assertDoesNotThrow(()-> new Graphe(tS, tA));            
+        }
+        
+        {
+            int[] tS = {1};
+            int[][] tA = {{1, 1}};
+            assertDoesNotThrow(()-> new Graphe(tS, tA));
+        }
+        
+        {   
+            int[] tS = {};
+            int[][] tA = {{}};
+            assertDoesNotThrow(()-> new Graphe(tS, tA));
+        }
+    }
+    
+    @Test
+    @DisplayName("Test du constructeur avec valeur INvalide")
+    void testGrapheNotOk() {
+        /* Il doit exister une liste de sommet et d'arcs */
+        assertThrows(IllegalArgumentException.class, ()->new Graphe(null, null));
+        
+        /* Liste d'arcs non vide alors que liste sommet vide */
+        int[] tsommet = {};
+        int[][] tarcs = {{1,2}};
+        assertThrows(IllegalArgumentException.class, ()->new Graphe(tsommet, tarcs));
+        
+        int[] tS = {1};
+        int[][] tA = {{2, 3}};
+        assertThrows(IllegalArgumentException.class,()-> new Graphe(tS, tA));
     }
 
     @Test
     void testGetNbArretes() {
-        fail("Not yet implemented");
+        //fail("Not yet implemented");
+        
     }
 
     @Test
     void testGetNbSommets() {
-        fail("Not yet implemented");
+        // fail 
     }
 
 }
