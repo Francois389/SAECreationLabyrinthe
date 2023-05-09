950ba0a82844806a38a3d304db4c1e17645d5909
Francois SP <francois-sp@gmx.fr>
2023-05-09 18:27:28 +0200

    Ajout de test à TestGraphe

--- Labyrinthe/src/test/representation/TestGraphe.java
+++ Labyrinthe/src/test/representation/TestGraphe.java
@@ -43,17 +43,37 @@ class TestGraphe {
     @Test
     @DisplayName("Test du constructeur avec valeur INvalide")
     void testGrapheNotOk() {
-        /* Il doit exister une liste de sommet et d'arcs */
+        /* Il doit exister une liste de sommet et/ou d'arcs */
         assertThrows(IllegalArgumentException.class, ()->new Graphe(null, null));
+        int[] tSommet = {1,2};
+        int[][] tArcs = {{1,2}};
+        assertThrows(IllegalArgumentException.class, ()->new Graphe(tSommet, null));
+        assertThrows(IllegalArgumentException.class, ()->new Graphe(null, tArcs));
         
         /* Liste d'arcs non vide alors que liste sommet vide */
-        int[] tsommet = {};
-        int[][] tarcs = {{1,2}};
+        int[] tsommet = {};      //STUB Il faut que la classe Sommet soit écrite pour pouvoir l'utiliser
+        int[][] tarcs = {{1,2}}; //STUB En attendant, nous utilisons des listes d'entiers
         assertThrows(IllegalArgumentException.class, ()->new Graphe(tsommet, tarcs));
         
+        /* Il existe une arrête entre deux sommet inexistant */
         int[] tS = {1};
         int[][] tA = {{2, 3}};
         assertThrows(IllegalArgumentException.class,()-> new Graphe(tS, tA));
+        
+        /* Un des sommets de l'arrête est inexistant*/
+        int[] tabS = {1};
+        int[][] tabA = {{1, 3}};
+        assertThrows(IllegalArgumentException.class,()-> new Graphe(tabS, tabA));
+        
+        /* Un des sommets de l'arrête est inexistant*/
+        tabA[0][0] = 3; //{{3,3}}
+        tabA[0][1] = 1; //{{3,1}}
+        assertThrows(IllegalArgumentException.class,()-> new Graphe(tabS, tabA));
+        
+        /* Il existe une boucle dans ce graphe */
+        tabA[0][0] = 1; //{{1,1}}
+        assertThrows(IllegalArgumentException.class,()-> new Graphe(tabS, tabA));
+        
     }
 
     @Test
