2fc3a3cb62d7adbc5de4f606970b965c09b5eb25
Francois SP <francois-sp@gmx.fr>
2023-05-10 17:01:04 +0200

    On continue les tests

--- Labyrinthe/src/representation/Graphe.java
+++ Labyrinthe/src/representation/Graphe.java
@@ -1,5 +1,7 @@
 package representation;
 
+import java.util.Iterator;
+
 //import  representation.Sommet;
 
 /**
@@ -18,7 +20,6 @@ public class Graphe {
     /** liste de tous le sommets */
     private int[][] listeArcs;
     
-    
     /**
      * Créer un graphe composé 
      * @param listeSommets
@@ -29,36 +30,64 @@ public class Graphe {
         
         if (!estValide(listeSommets,listeArcs)) {
             throw new IllegalArgumentException("Erreur - Paramétre du constructeur invalide");
-        }
-        
-        
-        
+        }     
+        // else
         this.listeSommet = listeSommets;
         this.listeArcs = listeArcs;
     }
     
+	/**
+	 * Fonction qui vérifie la validité du couple sommet - arcs, 
+	 * pour créer un graphe valide.
+	 * @param listeSommets
+	 * @param listeArcs
+	 * @return
+	 */
 	private boolean estValide(int[] listeSommets, int[][] listeArcs) {
-        boolean valide;
+        boolean estValide;
         
-        if (listeSommets == null && listeArcs == null) {
+        /* Un graphe doit forcement avoir un arc et un sommet dans notre situation */
+        if (   listeSommets == null  || listeArcs == null 
+            || listeArcs.length == 0 || listeArcs[0].length == 0 
+            || listeSommets.length == 0) {
             return false;
         }
-        valide = false;
-        for (int i = 0; i < listeSommets.length; i++ ){
-            for (int j = 0; j < listeArcs.length; j++) {
-                if (listeArcs[j][0] == listeSommets[i] || listeArcs[j][1] == listeSommets[i]) {
-                     valide = true;
-                 }   
+        
+        estValide = true;
+        /* Vérifie que tous les sommets des arcs existent */
+        for (int j = 0; j < listeArcs.length; j++) {
+            if (contient(listeSommets, listeArcs[j][0]) && contient(listeSommets,listeArcs[j][1])) {
+                estValide = true && estValide;
             }
-            if (!valide){
+        }
+        
+        /* On vérifie l'irreflexivitée du graphe */
+        for (int i = 0; i < listeArcs.length; i++) {
+            if (listeArcs[i][0] == listeArcs[i][1]) {
                 return false;
             }
         }
-        
         return true;
     }
 
-    /**
+	
+	/**
+	 * Renvoie true si element est dans liste.
+	 * False sinon.
+	 * @param liste
+	 * @param element
+	 * @return
+	 */
+	private static boolean contient(int[] liste, int element) {
+        for (int i = 0; i < liste.length; i++) {
+            if (liste[i] == element) {
+                return true;
+            }
+        }
+        return false; //STUB
+    }
+
+   /**
 	 * renvoie le nombre d'arcs du graphe
 	 * @return int nombre d'arcs
 	 */
--- Labyrinthe/src/test/representation/TestGraphe.java
+++ Labyrinthe/src/test/representation/TestGraphe.java
@@ -21,58 +21,79 @@ class TestGraphe {
     @Test
     @DisplayName("Test du constructeur avec valeur Valide")
     void testGrapheOK() {
-        {
+        {/* Un graphe normal */
             int[] tS = {1, 2, 3};
             int[][] tA = {{1, 2}, {2, 3}};
             assertDoesNotThrow(()-> new Graphe(tS, tA));            
         }
+             
+        { /* graphe symétrique */
+            int[] tS = {1, 2, 3};
+            int[][] tA = {{1, 2}, {2, 1}, {1, 3}, {3, 1}, {2, 3}, {3, 2}};
+            assertDoesNotThrow(()-> new Graphe(tS, tA)); 
+        } 
         
-        {
-            int[] tS = {1};
-            int[][] tA = {{1, 1}};
-            assertDoesNotThrow(()-> new Graphe(tS, tA));
-        }
+        { /* graphe transitif */
+            int[] tS = {1, 3, 4};
+            int[][] tA = {{1, 3}, {3, 4}, {4, 1}};
+            assertDoesNotThrow(()-> new Graphe(tS, tA)); 
+        }  
         
-        {   
-            int[] tS = {};
-            int[][] tA = {{}};
-            assertDoesNotThrow(()-> new Graphe(tS, tA));
-        }
+        { /* graphe avec un sommet isolé */
+            int[] tS = {1, 2, 3};
+            int[][] tA = {{1, 2}, {2, 1}};
+            assertDoesNotThrow(()-> new Graphe(tS, tA)); 
+        }  
+        
+       
     }
     
     @Test
     @DisplayName("Test du constructeur avec valeur INvalide")
     void testGrapheNotOk() {
-        /* Il doit exister une liste de sommet et/ou d'arcs */
-        assertThrows(IllegalArgumentException.class, ()->new Graphe(null, null));
-        int[] tSommet = {1,2};
-        int[][] tArcs = {{1,2}};
-        assertThrows(IllegalArgumentException.class, ()->new Graphe(tSommet, null));
-        assertThrows(IllegalArgumentException.class, ()->new Graphe(null, tArcs));
+        {/* Il doit exister une liste de sommet et/ou d'arcs */
+            assertThrows(IllegalArgumentException.class, ()->new Graphe(null, null));
+            int[] tSommet = {1,2};
+            int[][] tArcs = {{1,2}};
+            assertThrows(IllegalArgumentException.class, ()->new Graphe(tSommet, null));
+            assertThrows(IllegalArgumentException.class, ()->new Graphe(null, tArcs));            
+        }
         
-        /* Liste d'arcs non vide alors que liste sommet vide */
-        int[] tsommet = {};      //STUB Il faut que la classe Sommet soit écrite pour pouvoir l'utiliser
-        int[][] tarcs = {{1,2}}; //STUB En attendant, nous utilisons des listes d'entiers
-        assertThrows(IllegalArgumentException.class, ()->new Graphe(tsommet, tarcs));
+        {/* Liste d'arcs non vide alors que liste sommet vide */
+            int[] tsommet = {};      //STUB Il faut que la classe Sommet soit écrite pour pouvoir l'utiliser
+            int[][] tarcs = {{1,2}}; //STUB En attendant, nous utilisons des listes d'entiers
+            assertThrows(IllegalArgumentException.class, ()->new Graphe(tsommet, tarcs));
+        }
+
+        {/* Il existe une arrête entre deux sommet inexistant */
+            int[] tS = {1};
+            int[][] tA = {{2, 3}};
+            assertThrows(IllegalArgumentException.class,()-> new Graphe(tS, tA));            
+        }
         
-        /* Il existe une arrête entre deux sommet inexistant */
-        int[] tS = {1};
-        int[][] tA = {{2, 3}};
-        assertThrows(IllegalArgumentException.class,()-> new Graphe(tS, tA));
+        {/* Un des sommets de l'arrête est inexistant*/
+            int[] tabS = {1};
+            int[][] tabA = {{1, 3}};
+            assertThrows(IllegalArgumentException.class,()-> new Graphe(tabS, tabA));            
+        }
+
+        {/* Un des sommets de l'arrête est inexistant*/
+            int[] tabS = {1};
+            int [][] tabA = {{3,1}};
+            assertThrows(IllegalArgumentException.class,()-> new Graphe(tabS, tabA));            
+        }
+
+        {/* Il existe une boucle dans ce graphe */
+            int[] tabS = {1};
+            int [][] tabA = {{3,1}};
+            assertThrows(IllegalArgumentException.class,()-> new Graphe(tabS, tabA));            
+        }
         
-        /* Un des sommets de l'arrête est inexistant*/
-        int[] tabS = {1};
-        int[][] tabA = {{1, 3}};
-        assertThrows(IllegalArgumentException.class,()-> new Graphe(tabS, tabA));
-        
-        /* Un des sommets de l'arrête est inexistant*/
-        tabA[0][0] = 3; //{{3,3}}
-        tabA[0][1] = 1; //{{3,1}}
-        assertThrows(IllegalArgumentException.class,()-> new Graphe(tabS, tabA));
-        
-        /* Il existe une boucle dans ce graphe */
-        tabA[0][0] = 1; //{{1,1}}
-        assertThrows(IllegalArgumentException.class,()-> new Graphe(tabS, tabA));
+        {/* Un graphe doit  avoir au moins une arrête */
+            int[] tS = {1, 2, 4};
+            int[][] tA = {{}};
+            assertDoesNotThrow(()-> new Graphe(tS, tA));
+        }
         
     }
 
@@ -84,7 +105,13 @@ class TestGraphe {
 
     @Test
     void testGetNbSommets() {
-        // fail 
+        
+        {
+            int[] tS = {1, 3, 4};
+            int[][] tA = {{1, 3}, {3, 4}, {4, 1}};
+            Graphe g = new Graphe(tS, tA);
+            assertEquals(3, g.getNbSommets()); 
+        }
     }
 
 }
