ca77d0f526cd01ff19e16eda164614dfe20969b0
Francois SP <francois-sp@gmx.fr>
2023-05-11 09:19:48 +0200

    Ajout Sommet

--- Labyrinthe/src/representation/Graphe.java
+++ Labyrinthe/src/representation/Graphe.java
@@ -1,12 +1,13 @@
 package representation;
 
-import java.util.Iterator;
-
-//import  representation.Sommet;
+import outils.OutilsListe;
+import  representation.Sommet;
 
 /**
  * 
- * //TODO Commenter la responsabilités de la classe Graphe
+ * Classe représentant des Graphes étant 
+ * une liste de Sommets et 
+ * une liste de liste de deux sommets représentant les arcs
  * @author Quentin Costes
  * @author François de Saint Palais
  *
@@ -15,17 +16,17 @@ public class Graphe {
 
     //TODO des que la classe Sommet est fini changer les tableau de int en tableau de Sommet
     /** liste de tous le sommets */
-    private int[] listeSommet;
+    private Sommet[] listeSommet;
     
     /** liste de tous le sommets */
-    private int[][] listeArcs;
+    private Sommet[][] listeArcs;
     
     /**
      * Créer un graphe composé 
      * @param listeSommets
      * @param listeArcs
      */
-	public Graphe(int[] listeSommets, int[][] listeArcs) {
+	public Graphe(Sommet[] listeSommets, Sommet[][] listeArcs) {
         super();
         
         if (!estValide(listeSommets,listeArcs)) {
@@ -43,8 +44,7 @@ public class Graphe {
 	 * @param listeArcs
 	 * @return
 	 */
-	private boolean estValide(int[] listeSommets, int[][] listeArcs) {
-        boolean estValide;
+	private boolean estValide(Sommet[] listeSommets, Sommet[][] listeArcs) {
         
         /* Un graphe doit forcement avoir un arc et un sommet dans notre situation */
         if (   listeSommets == null  || listeArcs == null 
@@ -53,11 +53,11 @@ public class Graphe {
             return false;
         }
         
-        estValide = true;
         /* Vérifie que tous les sommets des arcs existent */
         for (int j = 0; j < listeArcs.length; j++) {
-            if (contient(listeSommets, listeArcs[j][0]) && contient(listeSommets,listeArcs[j][1])) {
-                estValide = true && estValide;
+            if (   !OutilsListe.contient(listeSommets, listeArcs[j][0]) 
+                || !OutilsListe.contient(listeSommets, listeArcs[j][1])) {
+                return false;
             }
         }
         
@@ -71,28 +71,14 @@ public class Graphe {
     }
 
 	
-	/**
-	 * Renvoie true si element est dans liste.
-	 * False sinon.
-	 * @param liste
-	 * @param element
-	 * @return
-	 */
-	private static boolean contient(int[] liste, int element) {
-        for (int i = 0; i < liste.length; i++) {
-            if (liste[i] == element) {
-                return true;
-            }
-        }
-        return false; //STUB
-    }
+	
 
    /**
 	 * renvoie le nombre d'arcs du graphe
 	 * @return int nombre d'arcs
 	 */
     public int getNbArretes() {
-        return this.listeArcs.length;
+        return listeArcs.length;
     }
     
     /**
@@ -100,6 +86,25 @@ public class Graphe {
      * @return int nombre de sommet
      */
     public int getNbSommets() {
-        return this.listeSommet.length;
+        return listeSommet.length;
+    }
+    
+    /**
+     * Permet de verifier la présence d'un arc entre deux sommets
+     * @param sommet1
+     * @param sommet2
+     * @return true si un arc existe entre les deux sommets, false sinon
+     */
+    public boolean sontRelies (Sommet sommet1, Sommet sommet2) {
+        
+        for (int i = 0; i < listeArcs.length; i++) {
+         	if (   listeArcs[i][0].equals(sommet1) && listeArcs[i][1].equals(sommet2)
+         	    || listeArcs[i][0].equals(sommet2) && listeArcs[i][1].equals(sommet1) ) {
+            	return true;
+            }  
+        }
+        
+        return false;
+        
     }
 }
