f88420586705edffc9b186938226c9341c28036b
Francois SP <francois-sp@gmx.fr>
2023-05-31 17:12:23 +0200

    Getter et Setter de position joueur + Test de Jeux

--- IterationQuatreLabyrinthe/src/representation/Jeux.java
+++ IterationQuatreLabyrinthe/src/representation/Jeux.java
@@ -6,9 +6,53 @@ package representation;
 
 /**
  * //TODO Commenter la responsabilités de la classe Jeux
- * @author francois
+ * @author de Saint Palais François
  *
  */
 public class Jeux extends Labyrinthe {
 
+    //TODO position joueur
+    /** La position en X du joueur dans le labyrinthe */
+    private int posXJoueur;
+    
+    /** La position en Y du joueur dans le labyrinthe */
+    private int posYJoueur;
+    
+    public Jeux(int hauteur, int largeur) {
+        super(hauteur, largeur);
+        // TODO Auto-generated constructor stub
+        setPosXJoueur(this.getEntre().getPosX());
+        setPosYJoueur(this.getEntre().getPosY());
+    }
+
+    /**
+     * @return la valeur de posXJoueur
+     */
+    public int getPosXJoueur() {
+        return posXJoueur;
+    }
+
+    /**
+     * @return la valeur de posYJoueur
+     */
+    public int getPosYJoueur() {
+        return posYJoueur;
+    }
+
+    /**
+     * @param posXJoueur modifie la valeur de posXJoueur
+     */
+    public void setPosXJoueur(int posXJoueur) {
+        this.posXJoueur = posXJoueur;
+    }
+
+
+    /**
+     * @param posYJoueur modifie la valeur de posYJoueur
+     */
+    public void setPosYJoueur(int posYJoueur) {
+        this.posYJoueur = posYJoueur;
+    }
+
+    
 }
---
+++ IterationQuatreLabyrinthe/src/test/application/jeu/TestJeux.java
@@ -0,0 +1,58 @@
+/**
+ * TestJeux.java                       31 mai 2023
+ * IUT de Rodez, pas de copyleft, pas de copyright
+ */
+package test.application.jeu;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * //TODO Commenter la responsabilités de la classe TestJeux
+ * @author de Saint Palais François
+ *
+ */
+class TestJeux {
+
+    /**
+     * Test method for {@link representation.Jeux#Jeux(int, int)}.
+     */
+    @Test
+    void testJeux() {
+        fail("Not yet implemented"); // TODO
+    }
+
+    /**
+     * Test method for {@link representation.Jeux#getPosXJoueur()}.
+     */
+    @Test
+    void testGetPosXJoueur() {
+        fail("Not yet implemented"); // TODO
+    }
+
+    /**
+     * Test method for {@link representation.Jeux#getPosYJoueur()}.
+     */
+    @Test
+    void testGetPosYJoueur() {
+        fail("Not yet implemented"); // TODO
+    }
+
+    /**
+     * Test method for {@link representation.Jeux#setPosXJoueur(int)}.
+     */
+    @Test
+    void testSetPosXJoueur() {
+        fail("Not yet implemented"); // TODO
+    }
+
+    /**
+     * Test method for {@link representation.Jeux#setPosYJoueur(int)}.
+     */
+    @Test
+    void testSetPosYJoueur() {
+        fail("Not yet implemented"); // TODO
+    }
+
+}
