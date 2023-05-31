2bf1547bcc031a33f2f80f2136e46f8e33630ed1
Lucas <lucasdesriaud6675@gmail.com>
2023-05-31 09:12:25 +0200

    ajout choix des dimensions

--- IterationQuatreLabyrinthe/src/application/Main.java
+++ IterationQuatreLabyrinthe/src/application/Main.java
@@ -20,21 +20,25 @@ public class Main {
      */
     public static void main(String[] args) {
         
-        final int CHOIX_QUITTER = 3;
-        final int CHOIX_CHAINE_ASCENDANTE = 1;
-        final int CHOIX_BACKTRACKING = 2 ;
+        final int CHOIX_QUITTER = 4;
+        final int CHOIX_DIMENSION = 1;
+        final int CHOIX_BACKTRACKING = 3 ;
+        final int CHOIX_CHAINE_ASCENDANTE = 2 ;
         
         final String MENU = """
                             +----------------------------+
-                                1. Chaine ascendante
-                                2. Backtracking
-                                3. Quitter
+                                1. Choix Dimension
+                                2. Chaine Ascendante
+                                3. Backtracking
+                                4. Quitter
                             +----------------------------+
                             """;
         Scanner analyseurChoix = new Scanner(System.in);
         
         int choix;
         boolean quitter;
+        
+        int[] DimensionLabyrinthe;
         
         choix = 0;
         quitter = false;
@@ -45,14 +49,19 @@ public class Main {
                 choix = analyseurChoix.nextInt();
                 switch (choix) {
                 case 1: {
-                    System.out.println("chaine ascendante");
+                    DimensionLabyrinthe = choixDimensions();
+                    System.out.println("Hauteur : " + DimensionLabyrinthe[0]  
+                                       + " \nLargeur : " + DimensionLabyrinthe[1]);      
                     break;
                 }
                 case 2: {
-                    System.out.println("backtracking");
+                    System.out.println("chaine ascendante");
                     break;
                 }
                 case 3: {
+                    System.out.println("backtracking");
+                }
+                case 4: {
                     quitter = true ;
                     break;
                 }
@@ -63,6 +72,37 @@ public class Main {
             }
             analyseurChoix.nextLine();
         } while (! quitter);
+    }
+
+    private static int[] choixDimensions() {
+
+        boolean hauteurValide ,
+                largeurValide;
+        
+        int [] DimensionChoisi = new int[2];
+        Scanner analyseurDimension = new Scanner(System.in);
+        hauteurValide = largeurValide = false ;
+        do {
+            System.out.print("Quelle hauteur souhaitez vous ? ");
+            if (analyseurDimension.hasNextInt()) {
+                DimensionChoisi[0] = analyseurDimension.nextInt();
+                analyseurDimension.nextLine();
+                hauteurValide = true ;
+                System.out.println("Hauteur choisi : " + DimensionChoisi[0]);
+            } else {
+                System.out.println("Hauteur invalide !");
+            }
+            System.out.print("Quelle largeur souhaitez vous ? ");
+            if (analyseurDimension.hasNextInt()) {
+                DimensionChoisi[1] = analyseurDimension.nextInt();
+                analyseurDimension.nextLine();
+                largeurValide = true ;
+                System.out.println("Largeur choisi : " + DimensionChoisi[1]);
+            } else {
+                System.out.println("Largeur invalide !");
+            }
+       } while (!hauteurValide && largeurValide);
+       return DimensionChoisi;
     }
 
 }
