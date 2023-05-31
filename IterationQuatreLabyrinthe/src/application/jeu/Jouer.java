f18612a81acc5864c0fa43e2d214183ace8427a5
Francois SP <francois-sp@gmx.fr>
2023-05-31 10:33:00 +0200

    Debut jeu

---
+++ IterationQuatreLabyrinthe/src/application/jeu/Jouer.java
@@ -0,0 +1,14 @@
+/**
+ * Jouer.java                       31 mai 2023
+ * IUT de Rodez, pas de copyleft, pas de copyright
+ */
+package application.jeu;
+
+/**
+ * //TODO Commenter la responsabilités de la classe Jouer
+ * @author francois
+ *
+ */
+public class Jouer {
+
+}
---
+++ IterationQuatreLabyrinthe/src/representation/Jeux.java
@@ -0,0 +1,14 @@
+/**
+ * Jeux.java                       31 mai 2023
+ * IUT de Rodez, pas de copyleft, pas de copyright
+ */
+package representation;
+
+/**
+ * //TODO Commenter la responsabilités de la classe Jeux
+ * @author francois
+ *
+ */
+public class Jeux extends Labyrinthe {
+
+}
